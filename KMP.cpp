a1ef1b63c108a01ea04a63cbfeb46bfe563e787c
FAhma <f.ahmadhem@gmail.com>
2020-09-08 03:22:25 +0200

    edited

--- KMP.cpp
+++ KMP.cpp
@@ -1,15 +1,25 @@
-
-    string pat="peek a boo";
-    string str="you speek a bootiful language";
-    int lps[pat.size()]={0};
-    for (int i =1,j=0; i <pat.size() ; ++i) {
-        if(pat[i]!=pat[j])j=0;
-        if(pat[i]==pat[j])lps[i]=++j;
-
-    }
-
-    for (int k = 0,i=0; k <str.size() ; ++k) {
-        if(str[k]!=pat[i])i=lps[i];
-        if(str[k]==pat[i])i++;
-        if(i==pat.size())cout<<k-(pat.size()-1)<<" ",i=lps[i-1];
-    }
+string s,t;
+int n=s.size(),m=pat.size();
+      int lps[n]={0};
+      int i=1,j=0;
+      while(i<m){
+          if(pat[i]==pat[j]){
+              lps[i]=++j;
+              i++;
+          }else {
+              if(!j)lps[i]=0,i++;
+              else j=lps[j-1];
+          }
+      }
+       i=0,j=0;
+      while(i<n){
+          if(pat[j]==s[i]){
+              ++j;
+              i++;
+          }
+          if(j==m)cout<<i-(j-1)-1<<" ",j=lps[j-1];
+          else if(i<n&&pat[j]!=s[i]) {
+              if(!j)i++;
+              else j=lps[j-1];
+          }
+      }
--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,38 +1,38 @@
 const int y[] = {0 , 0,  1 ,-1};// 8 shifts to neighbors
 const int x[] = {1, -1 , 0 , 0};// used in functions
 const int MAX = 1e2 + 5;
-int dist[MAX];
-vector<vector<pi>> adj(MAX);
-int vis[MAX][MAX], arr[MAX][MAX], n, m;
-bool valid(int i,int j){
-    return i>=0&&j>=0&&i<n&&j<m;
-}
+  int dist[MAX];
+  vector<vector<pi>> adj(MAX);
+  int vis[MAX][MAX], arr[MAX][MAX], n, m;
+  bool valid(int i,int j){
+      return i>=0&&j>=0&&i<n&&j<m;
+  }
 
-int dijkstra(int src, int t,int e) {
-    fill(dist, dist + MAX, INT_MAX - 10000);
-    priority_queue<pi, vector<pi>, greater<pi> > pq;
-    pq.push({0, src});
-    dist[src] = 0;
-    while (!pq.empty()) {
-        int d = pq.top().first;
-        int node = pq.top().second;
-            if (node == e) {
+  int dijkstra(int src, int t,int e) {
+      fill(dist, dist + MAX, INT_MAX - 10000);
+      priority_queue<pi, vector<pi>, greater<pi> > pq;
+      pq.push({0, src});
+      dist[src] = 0;
+      while (!pq.empty()) {
+          int d = pq.top().first;
+          int node = pq.top().second;
+              if (node == e) {
 
-                return d<=t;
-        }
-        pq.pop();
-        if (d > dist[node])continue;
-        for (int i = 0; i < adj[node].size(); ++i) {
-            pi v=adj[node][i];
-            if(dist[v.first]>dist[node]+v.second){
-                dist[v.first]=dist[node]+v.second;
-                pq.push({dist[node]+v.second,v.first});
-            }
-        }
-    }
+                  return d<=t;
+          }
+          pq.pop();
+          if (d > dist[node])continue;
+          for (int i = 0; i < adj[node].size(); ++i) {
+              pi v=adj[node][i];
+              if(dist[v.first]>dist[node]+v.second){
+                  dist[v.first]=dist[node]+v.second;
+                  pq.push({dist[node]+v.second,v.first});
+              }
+          }
+      }
 
-    return 0;
-}
+      return 0;
+  }
 
 /*************************************************/
 int main() {
