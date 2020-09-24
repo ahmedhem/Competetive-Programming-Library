5458ec114318c22193dc8e08df9e47e90473c01e
FAhma <f.ahmadhem@gmail.com>
2020-09-24 23:22:04 +0200

    t

---
+++ Modular inverse.cpp
@@ -0,0 +1,22 @@
+int extended_euclidean(int a, int b, int& x, int& y) {
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    int x1, y1;
+    int d = gcd(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - y1 * (a / b);
+    return d;
+}
+
+int x, y,a,b;
+int g = extended_euclidean(a, m, x, y);
+if (g != 1) {
+    cout << "No solution!";
+}
+else {
+    x = (x % m + m) % m;
+    cout << x << endl;
+}
