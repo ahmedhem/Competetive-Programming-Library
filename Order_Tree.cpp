fc08b0cc5dce5788c58663d6109281add0521ebf
ahmedhem <f.ahmadhem@gmail.com>
2020-12-19 15:59:54 +0200

    order stat

---
+++ Order_Tree.cpp
@@ -0,0 +1,29 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <bits/stdc++.h>
+
+typedef long long ll;
+#define endl "\n"
+using namespace __gnu_pbds;
+using namespace std;
+
+typedef
+tree<
+        pair<int, int>,
+        null_type,
+        less_equal<pair<int, int>>,
+        rb_tree_tag,
+        tree_order_statistics_node_update>
+        ordered_set;
+
+void init() {
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+}
+
+int main() {
+    init();
+
+
+
+}
